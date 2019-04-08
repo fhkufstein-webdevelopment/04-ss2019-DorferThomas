544a52b2dbc1f54c088a831b5f7519f90203ddf8
Your Name <you@example.com>
2019-04-08 22:25:56 +0200

    aufgabe 2 erledigt

--- js/user.js
+++ js/user.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-
-    var userListBody = $('.userList tbody');
-
     //@todo store and somehow update the current number of users
 
 
@@ -26,8 +23,48 @@ $(document).ready(function() {
         //6. maybe do something else... :-)
 
         //your code follows here
+        var userListBody = $('.userList tbody');
+        var usernameInput = $('#username');
+
+        //userNumber
+        var userNumber = userListBody.find('tr').length+1;
+
+
+
+        //creat the a new row with the input unserName
+        var row = $("<tr>");
+        row.append($(   "<td>" + userNumber + "</td>\n" +
+                        "<td>" + usernameInput.val() + "</td>\n" +
+                        "<td><button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button></td>\n" +
+                        "</tr>")
+        );
+
+        //create Button
+        var button = $("<button type=\"button\" class=\"btn btn-secondary btn-danger deleteTrigger\" title=\"Löschen\"><i class=\"fa fa-trash\"></i></button>");
+
+        //Create last Cell of Row + the Button
+        var lastCell = $("<td></td>");
+        lastCell.append(button);
+
+        userListBody.append(row);
+
+
+
 
         return false;
+    });
+
+
+    $('.deleteTrigger').click(function() {
+        //@todo
+        //1. remove current user from dom
+        //2. update number of current users
+
+        //your code follows here
+
+
+
+        deleteItem(this);
     });
 
 
